f7044cb6ff2db49432b6ac87d1452d16e136b5e8
Jose Plasencia <josejpd3@gmail.com>
2022-10-31 18:11:42 -0400

    use public

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 3001;
 const app = express();
 
 
-
+app.use(express.static("public"));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
 
