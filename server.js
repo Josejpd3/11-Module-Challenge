e25252faa28fbb97841d9e19164723f95263a513
Jose Plasencia <josejpd3@gmail.com>
2022-11-01 22:00:48 -0400

    fixed crypto const

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
-const { randomUUID } = require('crypto');
+const { crypto } = require('crypto');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
